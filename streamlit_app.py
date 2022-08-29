1c13067de07fa6ae09b3e872d472815833c50428
Adidam Sai Pranavi <112319479+adidampranavi@users.noreply.github.com>
2022-08-29 17:09:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('Menu')
 streamlit.header('My Smoothie')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
-
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect('Pick some',list(my_fruit_list.index));
