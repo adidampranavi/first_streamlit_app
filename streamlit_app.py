5b917723d9ec1b3506f8d00322f27123bf5c148b
Adidam Sai Pranavi <112319479+adidampranavi@users.noreply.github.com>
2022-08-29 17:18:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,5 @@ streamlit.header('My Smoothie')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 my_fruit_select=streamlit.multiselect('Pick some',list(my_fruit_list.index),['Avocado','Apple']);
-streamlit.dataframe(my_fruit_select)
+my_fruit_show=my_fruit_list.loc[my_fruit_select]
+streamlit.dataframe(my_fruit_show)
